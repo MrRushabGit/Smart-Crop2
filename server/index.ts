3b28688c770297a67e1e717d6b40f7826119adbc
MrRushabGit <engollarushab@gmail.com>
2026-04-21 21:54:04 +0530

    fix render deployment

--- server/index.ts
+++ server/index.ts
@@ -89,7 +89,7 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5006", 10);
+  const port = process.env.PORT || 10000;
   
 
   httpServer.listen(port, () => {
