7c0854e62993250f85dc158db80f79d6a5a0ae66
MrRushabGit <engollarushab@gmail.com>
2026-04-23 02:52:16 +0530

    final working backend config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
 
 export default {
   darkMode: ["class"],
@@ -103,13 +105,5 @@ export default {
       },
     },
   },
-  import animate from "tailwindcss-animate";
-  import typography from "@tailwindcss/typography";
-
-  export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [animate, typography],
-  } satisfies Config;
+  plugins: [animate, typography],
+} satisfies Config;
